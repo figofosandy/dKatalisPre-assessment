959b7d7cd32c6249914c059dafc9b5e74e932c25
figofosandy <figofosandy@gmail.com>
2019-11-06 16:40:06 +0700

    Initial Push

---
+++ Questions11.js
@@ -0,0 +1,14 @@
+//Problem statement
+//Given an Array of strings, use Array#reduce to create an object 
+//that contains the number of times each string occured in the array.
+
+var inputWords = ['Apple', 'Banana', 'Apple', 'Durian', 'Durian', 'Durian']
+
+function countWords(arr) {
+  return arr.reduce(function(countMap, word) {
+    // your implementation here 
+    countMap[word]=++countMap[word]||1
+    return countMap
+  }, {}) // second argument to reduce initialises countMap to {}
+}
+console.log(countWords(inputWords))
---
+++ React1Problem.js
@@ -0,0 +1,5 @@
+const MyComponent=function() {
+    return <div><h1>Test</h1></div>;
+  }
+  
+ReactDOM.render(MyComponent(), document.getElementById('root'));
